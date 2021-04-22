d18d74ef0e9465ddff8abaa7f328100b6bb1e6fd
Eivind Eriksen <mail@eivinderiksen.com>
2021-04-22 20:27:04 +0200

    Exporting Highlighter and added disabled property.

--- src/components/typography/Highlighter/Highlighter.js
+++ src/components/typography/Highlighter/Highlighter.js
@@ -1,9 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export const Highlighter = ({ term, children }) => {
+export const Highlighter = ({ term, children, disabled }) => {
     return React.Children.map(children, (child) => {
-        if (typeof child === "string") {
+        if (typeof child === "string" && !disabled) {
             const regex = new RegExp(term, "gi");
             const markedText = child.replace(regex, (match) => {
                 return `<mark>${match}</mark>`;
@@ -19,11 +19,20 @@ export const Highlighter = ({ term, children }) => {
     });
 };
 
+Highlighter.defaultProps = {
+    term: null,
+    disabled: false
+}
+
 Highlighter.propTypes = {
     /**
      * Regular expression that selects text that should be highlighted.
      */
     term: PropTypes.string,
+    /**
+     * Disable highlighting.
+     */
+    disabled: PropTypes.bool,
     /**
      * Any content. (Will only mark plain text and not text inside components).
      */
--- src/components/typography/index.js
+++ src/components/typography/index.js
@@ -1,3 +1,4 @@
 export * from "./Code";
+export * from "./Highlighter";
 export * from "./Paragraph";
 export * from "./Text";

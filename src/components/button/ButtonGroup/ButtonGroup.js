ba5c7911b2f6063f822c86f3492013ae90d82889
Eivind Eriksen <mail@eivinderiksen.com>
2021-11-11 09:41:48 +0100

    ButtonGroup fill. PageHero color variables.

--- src/components/button/ButtonGroup/ButtonGroup.js
+++ src/components/button/ButtonGroup/ButtonGroup.js
@@ -5,11 +5,12 @@ import styles from "./ButtonGroup.scss";
 
 import { Button } from "../Button";
 
-export const ButtonGroup = ({ children, className }) => {
+export const ButtonGroup = ({ fill, children, className }) => {
     // Button elements
     const buttons = React.Children.map(children, (child) => {
         if (child && child.type === Button) {
             return React.cloneElement(child, {
+                fill: fill,
                 className: styles.item
             });
         }
